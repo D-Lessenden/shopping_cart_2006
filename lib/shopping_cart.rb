2d7dc7ad54017897344b72d2095f11778d914cbf
Daniel Lessenden <dless27@gmail.com>
2020-07-08 11:52:42 -0500

    Update for Iteration 3

--- lib/shopping_cart.rb
+++ lib/shopping_cart.rb
@@ -21,10 +21,56 @@ attr_reader :name
   def details
     details = {}
     details[:name] = "#{@name}"
-    details[:capacity] = @capacity[/\d+/].to_i 
+    details[:capacity] = @capacity[/\d+/].to_i
     p details
+  end
+
+  def total_number_of_products
+    # total = []
+    # @product.each do |x|
+    # end
+  #  @product1.quantity
+  #  @quantity
+    #@product.add_product(product1)
+  #  @product.length
+    #@product[0].capacity
+    @sums = []
+    @product.each do |x|
+      #require 'pry'; binding.pry
+      @sums << x.quantity
+    end
+#p sums
+  @sums.sum
 
 
   end
+
+  def is_full?
+    @sums.sum >= 30
+  end
+
+  def products_by_category(type)
+    new_cat = []
+    @product.each do |x|
+      #require 'pry'; binding.pry
+      new_cat << if x.category == type
+      end
+    end
+  end
+
+
+
+    # @product.select do |type|
+    #   type == ":paper"
+    # end
+
+
+    # [@product[0], @product[2]]
+    # cats = []
+    # @product.each do |cat|
+    #   require 'pry'; binding.pry
+    #   cats << cat.category == :paper
+
+
 
 end
